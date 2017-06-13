fa7aabe89b34c04df87d0a260b3fe02ad5fa377d
DzvinkaYarish <yarish@ucu.edu.ua>
2017-06-13 11:53:19 +0300

    more places from  fb

--- facebook_miner_get_places.py
+++ facebook_miner_get_places.py
@@ -5,18 +5,40 @@ FACEBOOK_ACCESS_TOKEN = "680740112135077%7CEq_CUNKKEzblfj8bkkn6GtKV1cI"
 def get_places():
     url ="https://graph.facebook.com/v2.9/search"
     # list of all fields https://developers.facebook.com/docs/places/fields
-    args = urllib.parse.urlencode({"type": "place", "center": "49.8397,24.0297", "distance": "1000", "fields": "name,checkins,picture", "access_token": FACEBOOK_ACCESS_TOKEN, "limit":100
-})
-    #print(url + "?" + args)
-    x = urllib.request.urlopen(url + "?" + args)
-    data = json.loads(x.read().decode("utf-8"))['data']
+    #"49.8397,24.0297"
+
     parsed_data = {}
 
-    for place in data:
-        parsed_data[place['name']] = 'https://www.facebook.com/pg/%s/reviews/' % place['id']
+
+    for k in range(2):
+        lat = 49.8397
+        longit = 24.0297
+        for i in range(10):
+            for j in range(10):
+
+
+                coord = str(lat) + "," + str(longit)
+                args = urllib.parse.urlencode({"type": "place", "center": coord, "distance": "200", "fields": "name,checkins,picture", "access_token": FACEBOOK_ACCESS_TOKEN, "limit":100
+            })
+                #print(url + "?" + args)
+                x = urllib.request.urlopen(url + "?" + args)
+                data = json.loads(x.read().decode("utf-8"))['data']
+
+
+                for place in data:
+                    parsed_data[place['name']] = 'https://www.facebook.com/pg/%s/reviews/' % place['id']
+
+                if (k == 0):
+                    lat += 0.005
+                    longit += 0.005
+                else:
+                    lat -= 0.005
+                    longit -= 0.005
+
+
 
     return parsed_data
 
 places = get_places()
 print(places)
-#print(len(places))
+print(len(places))

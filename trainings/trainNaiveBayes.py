14d1a0637b6535b9c65f37d1f7f9d29708037c01
DzvinkaYarish <yarish@ucu.edu.ua>
2017-06-14 17:46:32 +0300

    more places from  fb

--- trainings/trainNaiveBayes.py
+++ trainings/trainNaiveBayes.py
@@ -129,7 +129,7 @@ if __name__ == "__main__" :
 
     #dev_set = [(tip["text"], tip["authorInteractionType"]) for idd in json_odesa_tips for tip in json_odesa_tips[idd]["tips"]]
 
-    dev_set = [(tip, "very good") for tip in feature_set_very_good[600:700]]
+    dev_set = [(tip, "very good") for tip in feature_set_very_good[600:1000]]
     errors = []
     for (tip, label) in dev_set[:100]:
         guess = classifier.classify(tip_features(tip, dict_most_freq))

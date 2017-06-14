fd9ea123c98b7423cb9ae9d8f68633bede29be4c
DzvinkaYarish <yarish@ucu.edu.ua>
2017-06-14 18:04:49 +0300

    more places from  fb

--- trainings/trainNaiveBayes.py
+++ trainings/trainNaiveBayes.py
@@ -140,8 +140,8 @@ if __name__ == "__main__" :
 
     print("accuracy %s" % str(nltk.classify.accuracy(classifier, test_set)))
     print(classifier.classify(
-            tip_features(
-                "Фірмове пиво не сподобалось. Несмачне. Загалом в закладі погано. Часом важко пересуватись між столиками.",
+            tip_features(process_tip(
+                "Фірмове пиво не сподобалось. Несмачне. Загалом в закладі погано. Часом важко пересуватись між столиками.", ukr_stop_words),
                 dict_most_freq)))
 
 
